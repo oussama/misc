8a810b11d4795379862d54768d4ed790cb3e4484
Oussama Gammoudi <md.oussama@gmail.com>
2016-10-02 02:26:09 +0100

    Update rancher-server.sh

--- rancher-server.sh
+++ rancher-server.sh
@@ -5,3 +5,5 @@ docker run -d \
     -e "LETSENCRYPT_EMAIL=oussama_md@hotmail.com" \
      --restart=unless-stopped --expose 8080 \
     rancher/server
+
+sudo docker run -d -v /data/rancher/mysql:/var/lib/mysql  -v /data/rancher/cert.crt:/ca.crt --restart=unless-stopped -p 8080:8080 rancher/server
