d852aaa63f62f03529461456a56451f5cc422160
Oussama Gammoudi <md.oussama@gmail.com>
2016-11-26 03:39:34 +0100

    Update traefik-install.sh

--- k8s/traefik-install.sh
+++ k8s/traefik-install.sh
@@ -1,6 +1,6 @@
 openssl req -newkey rsa:2048 -nodes -keyout tls.key -x509 -days 365 -out tls.crt
-kubectl create secret generic traefik-cert --from-file=tls.crt --from-file=tls.key --namespace=kube-system
+kubectl apply secret generic traefik-cert --from-file=tls.crt --from-file=tls.key --namespace=kube-system
 wget -N https://raw.githubusercontent.com/oussama/misc/master/k8s/traefik.toml
-kubectl create configmap traefik-conf --from-file=traefik.toml --namespace=kube-system
+kubectl apply configmap traefik-conf --from-file=traefik.toml --namespace=kube-system
 wget -N https://raw.githubusercontent.com/oussama/misc/master/k8s/traefik.yaml
-kubectl create -f traefik.yaml
+kubectl apply -f traefik.yaml
