74ba6f544aeb1b84f9712661bf624f14217c0c6d
Oussama Gammoudi <md.oussama@gmail.com>
2016-11-26 05:04:13 +0100

    Update traefik-install.sh

--- k8s/traefik-install.sh
+++ k8s/traefik-install.sh
@@ -1,5 +1,7 @@
 openssl req -newkey rsa:2048 -nodes -keyout tls.key -x509 -days 365 -out tls.crt
-kubectl apply secret generic traefik-cert --from-file=tls.crt --from-file=tls.key --namespace=kube-system
+kubectl create secret generic traefik-cert --from-file=tls.crt --from-file=tls.key --namespace=kube-system
+kubectl delete secret traefik-cert --namespace=kube-system
+
 wget -N https://raw.githubusercontent.com/oussama/misc/master/k8s/traefik.toml
 kubectl delete configmap traefik-conf --namespace=kube-system
 
@@ -12,3 +14,4 @@ kubectl describe deploy traefik-ingress-controller --namespace=kube-system
 kubectl --namespace=kube-system get node
 kubectl describe node traefik-ingress-controller --namespace=kube-system
 kubectl describe po traefik-ingress-controller --namespace=kube-system
+kubectl logs traefik-ingress-controller --namespace=kube-system
